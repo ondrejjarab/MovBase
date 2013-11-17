8c84c7fe047841ad08eee1d8d1b2c611400ebbc5
ondrejjarab <ondrej.jarab@gmail.com>
2013-11-17 21:20:44 +0100

    nejake controlleri

---
+++ src/java/sk/movbase/controllers/CommentController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sk.movbase.controllers;
+
+import java.util.Collection;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sk.movbase.jpaControllers.FilmJpaController;
+import sk.movbase.models.Comment;
+
+/**
+ *
+ * @author Ondrej
+ */
+@Controller
+@RequestMapping("/movie/{id}/comments")          //komentare pre film/serial s id ...
+public class CommentController {
+    
+    public String allComments(@PathVariable int movieId, ModelMap model) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MovBasePU");
+        FilmJpaController fJpa = new FilmJpaController(emf);
+        Collection<Comment> comments = fJpa.findFilm(movieId).getCommentCollection();
+        model.addAttribute("comments", comments);
+        return "comment/index";
+    }
+    
+    public String newComment() {
+       
+    return "comment/new";
+}
+    
+    public String edit() {
+        return "comment/edit";
+    }
+    
+    public String create(@PathVariable int movieId) {
+        return null;
+    }
+    
+    public String update(@PathVariable int movieId) {
+        return null;
+    }
+    
+    
+    
+}
